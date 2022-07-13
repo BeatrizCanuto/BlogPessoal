e467a465ecb38f74f95c5a149eb7793831c0e1f4
BeatrizCanuto <BeatrizCanuto>
2022-07-12 22:02:44 -0300

    Atualização security

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -39,16 +39,21 @@ public class Usuario {
 	
     private String foto;
     
+    private String tipo;
+    
 	@OneToMany (mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List <Postagem> postagem;
 
-	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+	
+
+	public Usuario(Long id, String nome, String usuario, String senha, String foto, String tipo) {
 		this.id = id;
 		this.nome = nome;
 		this.usuario = usuario;
 		this.senha = senha;
 		this.foto = foto;
+		this.tipo = tipo;
 	}
 
 	public Usuario() {	}
@@ -93,7 +98,13 @@ public class Usuario {
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
-    
+	public String getTipo() {
+		return this.tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 	public List<Postagem> getPostagem() {
 		return this.postagem;
 	}
